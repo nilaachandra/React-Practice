9113c377efe4b5a1e471145ed8bed74d9aa4a1e0
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-02-05 22:46:55 +0530

    ddd

--- ReactPractice/src/App.jsx
+++ ReactPractice/src/App.jsx
@@ -4,12 +4,12 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <diV className="flex justify-around items-center mt-[2vw]">
+      <div className="flex justify-around items-center mt-[2vw]">
         <Card title="Hello Bro" description="Welcome to this website" />
         <Card title="Hello Bro 2" description="Welcome to this website" />
         <Card title="Hello Bro 3" description="Welcome to this website" />
         <Card title="Hello Bro 4" description="Welcome to this website" />
-      </diV>
+      </div>
     </>
   );
 }
