0c644e81d8235e21d02ef86609f8e5d0db4c0f87
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-02-05 22:57:15 +0530

    dd

--- ReactPractice/src/components/Card.jsx
+++ ReactPractice/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = (props) => {
     return(
-        <div className="card h-[20vh] w-[40vh] border border-black">
+        <div className="card h-[20vh] w-[40vh] border border-red">
             <h1>{props.title}</h1>
             <p>{props.description}</p>
         </div>
