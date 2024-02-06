04601ec79bddb15b3b7c5d076aec92520e071f63
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-02-06 21:34:04 +0530

    reset button added

--- ReactPractice/src/components/Counter.jsx
+++ ReactPractice/src/components/Counter.jsx
@@ -6,7 +6,9 @@ const Counter = () => {
     const incrementCount = () => {
         setCount(count + 1);
     };
-
+    const resetCount = () => {
+        setCount(0)
+    }
     const decrementCount = () => {
         setCount(count - 1);
     };
@@ -16,6 +18,7 @@ const Counter = () => {
             <h2>This is my Counter</h2>
             <p className="border border-black bg-[beige] w-[10%]">Count: {count}</p>
             <button onClick={decrementCount} className="border border-black m-4 h-8 w-8" >-</button>
+            <button onClick={resetCount} className="border border-black m-4 h-8 w-12" >Reset</button>
             <button onClick={incrementCount} className="border border-black m-4 h-8 w-8" >+</button>
         </div>
     )
